cd6c0b25318ec6f0760574fe96856725005ef155
BOOK-CI93RAR7FF\87062 <22000286@handong.ac.kr>
2023-09-03 13:24:15 +0900

    Create interface

--- src/main/java/org/WordMaster/ICRUD.java
+++ src/main/java/org/WordMaster/ICRUD.java
@@ -1,5 +1,8 @@
 package org.WordMaster;
 
 public interface ICRUD {
-
+    public Object add();
+    public int update(Object obj);
+    public int delete(Object obj);
+    public void selectOne(int id);
 }
